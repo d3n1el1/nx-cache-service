2260ab2b79865d2a28ee35349cbfaa76e9a06edc
Daniel Ioanu <d3n1el@live.com>
2026-08-29 18:35:08 +0200

    Implement PUT cache API request

--- internal/api/cache.go
+++ internal/api/cache.go
@@ -30,8 +30,6 @@ func handleCacheGet(store cache.Store, log *slog.Logger) http.Handler {
 		if err != nil {
 			if !errors.Is(err, cache.ErrNotFound) {
 				log.Error("cache get failed", "project", project, "hash", hash, "err", err)
-
-				return
 			}
 
 			_ = writeErrorResponse(w, http.StatusNotFound, "Not Found", nil)
@@ -53,17 +51,31 @@ func handleCacheGet(store cache.Store, log *slog.Logger) http.Handler {
 
 func handleCachePut(store cache.Store, log *slog.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := validatePathParam("project", r.PathValue("project")); err != nil {
+		project := r.PathValue("project")
+		hash := r.PathValue("hash")
+
+		if err := validatePathParam("project", project); err != nil {
 			_ = writeErrorResponse(w, http.StatusBadRequest, err.Error(), nil)
 			return
 		}
 
-		if err := validatePathParam("hash", r.PathValue("hash")); err != nil {
+		if err := validatePathParam("hash", hash); err != nil {
 			_ = writeErrorResponse(w, http.StatusBadRequest, err.Error(), nil)
 			return
 		}
 
-		_ = writeErrorResponse(w, http.StatusNotImplemented, "Not implemented", nil)
+		err := store.Put(r.Context(), project, hash, r.Body)
+
+		if err != nil {
+			if !errors.Is(err, cache.ErrExists) {
+				log.Error("cache already exists and cannot be overriden", "project", project, "hash", hash, "err", err)
+			}
+
+			_ = writeErrorResponse(w, http.StatusConflict, err.Error(), nil)
+			return
+		}
+
+		_ = writeJSON(w, http.StatusOK, struct{}{})
 	})
 }
 
